a263ad661848435bd9509498d6232de352180ae1
angelo <mor.angelo.mor@gmail.com>
2016-01-11 17:15:00 +0100

    initial commit

---
+++ draggablecomponents/src/main/java/me/bemind/draggablecomponents/DraggableLayout.java
@@ -0,0 +1,22 @@
+package me.bemind.draggablecomponents;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by angelomoroni on 11/01/16.
+ */
+public class DraggableLayout extends FrameLayout {
+    public DraggableLayout(Context context) {
+        super(context);
+    }
+
+    public DraggableLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DraggableLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
